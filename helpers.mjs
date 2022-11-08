7b73bcf065cf8b114bba1a7c23770204d092829a
adb-sh <git@adb.sh>
2022-11-08 12:33:09 +0100

    Debug

--- helpers.mjs
+++ helpers.mjs
@@ -3,5 +3,5 @@ Titel: ${ ticket.subject }
 Beschreibung: ${ ticket.description }
 Status: ${ ticket.status?.name ?? '- kein Status -' }
 Creator: ${ ticket.owner?.full_name }
-Zugewiesen an: ${ ticket.assigned_to?.map(user => user.full_name).join(', ') ?? '- nicht zugewiesen -' }
+Zugewiesen an: ${ ticket.assigned_to?.full_name ?? '- nicht zugewiesen -' }
 `;
